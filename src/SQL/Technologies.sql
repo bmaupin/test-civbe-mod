913c29dfa6643202c3ff060e5aea84ec1e0000c2
bmaupin <bmaupin@users.noreply.github.com>
2026-04-20 13:09:33 -0400

    feat: Make protogenetics not a hybrid tech
    It's the only hybrid leaf tech

--- src/SQL/Technologies.sql
+++ src/SQL/Technologies.sql
@@ -1,3 +1,24 @@
+-- NOTE: This and any other changes to Technology_Affinities need to happen before the
+-- the affinity values are adjusted below
+--
+-- Protogenetics is the only hybrid leaf tech, so move one of the affinities to avoid
+-- hybrid affinities
+UPDATE Technology_Affinities
+SET TechType = 'TECH_VERTICAL_FARMING'
+WHERE TechType = 'TECH_PROTOGENETICS'
+  AND AffinityType = 'AFFINITY_TYPE_PURITY'
+  AND EXISTS (SELECT Description FROM Civilizations WHERE Type = 'CIVILIZATION_CHUNGSU');
+
+-- Update the cost to keep the techs balanced for each affinity
+UPDATE Technologies
+SET COST = (
+  SELECT COST FROM Technologies WHERE Type = 'TECH_PROTOGENETICS'
+)
+WHERE Type = 'TECH_VERTICAL_FARMING'
+  AND EXISTS (SELECT Description FROM Civilizations WHERE Type = 'CIVILIZATION_CHUNGSU');
+
+
+
 -- Remove affinity points from branch techs because we don't want to block any branch
 -- techs and we also don't want any hybrid affinities
 DELETE FROM Technology_Affinities
