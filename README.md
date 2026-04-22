e4a9d83e51b1641b69795786c580c693922e9589
bmaupin <bmaupin@users.noreply.github.com>
2026-04-22 13:36:35 -0400

    feat: Make affinity resource tile improvements affinity-specific
    This will further make factions asymmetric
    
    (This commit was cherry-picked from https://github.com/bmaupin/beyond-earth-eclipse)

--- README.md
+++ README.md
@@ -6,5 +6,22 @@
 Features disabled
 
 - Affinity quests and quests that reference units we modify
+- Diplomatic agreements?
+- Diplomatic traits?
+- Air and sea units?
+- Water cities
+- Maps with
+
+## Features
+
+### Affinities
+
+#### Affinities have unique tile improvements
+
+Some tile improvements have been made affinity-specific to increase asymmetric gameplay:
+
+- Harmony: Biowell, Xenomass Well
+- Purity: Dome, Floatstone Quarry
+- Supremaxy: Node, Firaxite Mine
 
 -->
--- src/SQL/Builds.sql
+++ src/SQL/Builds.sql
@@ -1,7 +1,31 @@
+-- The Node improvement is only available to supremacy, so make these improvements
+-- affinity-specific as well for more balanced and asymmetric gameplay
+-- Harmony
 UPDATE Builds
 SET PrereqTech = 'TECH_TISSUE_ENGINEERING'
 WHERE Type = 'BUILD_BIOWELL';
 
+-- Purity
 UPDATE Builds
 SET PrereqTech = 'TECH_CIVIL_SUPPORT'
 WHERE Type = 'BUILD_DOME';
+
+
+-- TODO: Test; if affinity resources/units take too long to get to, we could move these
+--       to earlier leaf techs (TECH_ALIEN_LIFEFORMS, TECH_BALLISTICS, TECH_POWER_SYSTEMS)
+-- Limit the tile improvements for affinity resources to the corresponding affinity by
+-- moving each one from the branch tech to the affinity-specific leaf tech under it
+-- Harmony
+UPDATE Builds
+SET PrereqTech = 'TECH_ALIEN_ADAPTATION'
+WHERE Type = 'BUILD_XENOMASS_WELL';
+
+-- Purity
+UPDATE Builds
+SET PrereqTech = 'TECH_BIOSPHERES'
+WHERE Type = 'BUILD_FLOAT_STONE_QUARRY';
+
+-- Supremacy
+UPDATE Builds
+SET PrereqTech = 'TECH_TACTICAL_ROBOTICS'
+WHERE Type = 'BUILD_FIRAXITE_MINE';
