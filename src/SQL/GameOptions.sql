2685375995587f44a7c7f51599d1fd7d922bddf0
bmaupin <bmaupin@users.noreply.github.com>
2026-05-02 19:21:45 -0400

    feat: Remove game options for water starts

--- src/SQL/GameOptions.sql
+++ src/SQL/GameOptions.sql
@@ -1,3 +1,5 @@
--- If Mini Beyond Earth is installed, remove the Auto Upgrade Units game option
 DELETE FROM GameOptions
-WHERE Type = 'GAMEOPTION_AUTO_UPGRADE_UNITS';
+WHERE Type = 'GAMEOPTION_ALL_ADJACENT_OR_WATER_STARTS'
+  OR Type = 'GAMEOPTION_ALL_IN_WATER_STARTS'
+  -- If Mini Beyond Earth is installed, remove the Auto Upgrade Units game option
+  OR Type = 'GAMEOPTION_AUTO_UPGRADE_UNITS';
